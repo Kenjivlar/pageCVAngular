e4ca5bcce6de30f4f3d3b4088f2743d7889f5234
Kevin G <kev79asl@gmail.com>
2024-05-23 23:14:56 -0600

    Update Image Modal Dialog

--- src/app/pages/dialog-pic/dialog-pic.component.ts
+++ src/app/pages/dialog-pic/dialog-pic.component.ts
@@ -9,5 +9,24 @@ import { DialogRef } from '@angular/cdk/dialog';
   styleUrl: './dialog-pic.component.css'
 })
 export class DialogPicComponent {
-  constructor(public dialogRef: DialogRef){}
+
+  slide: string[];
+  i: number;
+  
+  constructor(public dialogRef: DialogRef){
+    this.slide = ['../../../assets/img/goallist.png', '../../../assets/img/goal2.png', '../../../assets/img/goal1.png'];
+    this.i = 0;
+  }
+
+  showSlide(){
+    return this.slide[this.i];
+  }
+
+  prevSlide(){
+    this.i == 0 ? (this.i = this.slide.length - 1) : this.i--;
+  }
+
+  nextSlide(){
+    this.i < this.slide.length - 1 ? this.i++ : (this.i = 0);
+  }
 }
